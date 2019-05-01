5746433db1110d3f8ddbbb111d680bde49634693
Alex <alexcc17@gmail.com>
2019-05-01 12:08:40 -0400

    Initial Commit

---
+++ Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Alex on 5/1/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
